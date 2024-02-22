cb66fbd55ddbc1410d49e2b977b38db2e0756d50
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-02-22 11:24:17 -0500

    refactor

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -15,23 +15,23 @@ export class ProductsController {
     return this.router;
   }
 
-  async findAll(request: Request, response: Response) {
+  private async findAll(request: Request, response: Response) {
     const result = await axios.get<ProductResponse>(URL);
     response.json(result.data.products);
   }
 
-  async find(request: Request, response: Response) {
+  private async find(request: Request, response: Response) {
     const id = Number(request.params['id']);
     const result = await axios.get<Product>(`${URL}/${id}`);
     response.json(result.data);
   }
 
-  async findCategories(request: Request, response: Response) {
+  private async findCategories(request: Request, response: Response) {
     const result = await axios.get<string[]>(`${URL}/categories`);
     response.json(result.data);
   }
 
-  async findByCategory(request: Request, response: Response) {
+  private async findByCategory(request: Request, response: Response) {
     const category = String(request.params['category']);
     const result = await axios.get<ProductResponse>(`${URL}/category/${category}`);
     response.json(result.data.products);
